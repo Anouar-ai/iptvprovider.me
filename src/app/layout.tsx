99c75263f7c0121a22157e41c32448f878a55970
unknown <aaissaoui@alaska.edu>
2025-12-23 15:19:43 +0100

    Update package files and add sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -184,6 +184,17 @@ export default function RootLayout({
         <link rel="preconnect" href="https://image.tmdb.org" />
         {/* Google Analytics handled by Analytics component in body */}
         
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TS669NMN');
+          `}
+        </Script>
+        
         {/* Google Ads Tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17824278334"
@@ -199,6 +210,15 @@ export default function RootLayout({
         </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TS669NMN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <ProgressBar />
         <Analytics />
         <Script
